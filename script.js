877a5e33b20a90446068e19cd1008508ec455c9b
Amal Shibu <87291689+AmalShibu888@users.noreply.github.com>
2022-12-23 09:31:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -235,74 +235,136 @@ let options = {
     "Zebra"]
 };
 
-class PlayerVerification{
-    initialisation(){
-        this.winCount = 0;
-        this.failedAttempt = 0;
-        this.userName;
-        this.password;
-        return this;
-    }
-
-    login(userName, Password){
-
-    }
-    logout(){
-
-    }
-}
 
 
 
-class Game extends PlayerVerification{
 
-    // function for getting a random word from the database
+
+
+// class to select hide the word
+
+class SelectedWord{
+
+    // get a random word from the database
     getWord(){
         const keyss = Object.keys(options);
         this.category = keyss[Math.floor(Math.random()*keyss.length)];
         const arr = options[this.category];
         this.word = arr[Math.floor(Math.random()*arr.length)];
-        console.log(this.category);
+        console.log(this.word);
+        this.hideWord();
+        this.noOfRepeat();
         return this;
     }
 
-}
 
+    // hiding the word and and marking each letter with underline
 
-
-class SelectedWord extends Game{
     hideWord(){
-        this.getWord();
         const el = document.querySelector('.word');
         let s = this.word;
-        console.log(s);
         for(let i = 0;i<s.length;i++){
             const x = document.createElement('span');
             if(s[i] === " ")
                 x.className = "letter space";
             else
                 x.className = "letter S" + s[i].toLowerCase();
-            // x.textContent = s[i];
             el.appendChild(x);
             
         }
         return this;
     }
 
+
+    // returning the number of times a letter is present in the word selected by the getword function into repeat object 
+
     noOfRepeat(){
         this.repeat = {'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0};
         let s = this.word;
-        console.log(s);
         for(let i = 0;i<s.length;i++)
             this.repeat[s[i].toLowerCase()]++;
         
         this.fail = 0;
         let spaces = this.word.split(' ').length - 1;
         this.success = this.word.length - spaces;
-            // console.log(this.repeat)
         return this;
     }
 
+    
+
+}
+
+
+
+
+// upating and functioning both the inbuilt as well as taking input from the analog keyboard
+
+class keyboard extends SelectedWord{
+
+
+    // update the status of each key in the keyboard and let us know wheather it is pressed or not
+    updateKeyboard(s , node){
+        if(this.repeat[s.toLowerCase()] >= 0)
+        {
+            console.log(this.success);
+            if(this.repeat[s.toLowerCase()] > 0)
+            {
+                node.className += " rightLetter";
+                this.updateLetter(s);
+            }
+            else
+            {
+                node.className += " wrongLetter";
+                this.fail++;
+                this.updateFigure(this.fail);
+                this.updateWrongLetter(s);
+            }
+            this.repeat[s.toLocaleLowerCase()] = -1;
+            if(this.fail == 6){
+                alert('Sorry you are hanged !!!!');
+                this.reset();
+            }
+            else if(this.success == 0){
+                alert('yay you won !!!!');
+                this.reset();
+            }
+        }
+    }
+
+
+    // taking the input and take into account of the functioning of the digital keyboard
+
+    digitalPress(){
+        const digitalKeyboard = document.querySelectorAll(".alphaKeys");
+        // console.log(digitalKeyboard);
+        digitalKeyboard.forEach((e)=>{
+            e.addEventListener('click',(x)=>{
+                this.updateKeyboard(e.textContent , e);
+                
+                
+            })
+        })
+        return this;
+    }
+
+
+    // taking the input and take into account of the functioning of the analog keyboard
+
+    analogPress(){
+        document.addEventListener('keypress' , (e)=>{
+            if((e.charCode>64 && e.charCode <91) || (e.charCode >96 && e.charCode <123))
+            {
+                this.updateKeyboard(e.key , document.querySelector('.' + e.key.toLowerCase()));
+            }
+        })
+    }
+}
+
+
+// updating the figure the analog keyboard and the wrong letters slots with each press
+class updatingReset extends keyboard{
+
+    // updating the figure in accordance with the current state
     updateFigure(x){
         if(x == 1){
             let x = document.querySelector('.try1');
@@ -331,18 +393,29 @@ class SelectedWord extends Game{
         }
     }
 
+
+    // makes the character visible and decrease the success parameter by 1
+
     updateLetter(s){
-        let x = document.querySelectorAll('.S'+s.toLocaleLowerCase());
+        let x = document.querySelectorAll('.S'+s.toLowerCase());
         x.forEach(e => {
             e.textContent = s.toUpperCase();
             this.success--;
         });
         
     }
+
+
+
+
+    // makes the wrong letters popup in the wrong letters section
     updateWrongLetter(s){
         let x = document.querySelector('.wrongLetterList');
         x.textContent += " "+s;
     }
+
+
+    // resets the entire game takes and takes new random word and the game starts again
 
     reset(){
         this.fail = 0;
@@ -366,68 +439,34 @@ class SelectedWord extends Game{
         }
         x = document.querySelector('.wrongLetterList');
         x.textContent = "";
-        this.hideWord().noOfRepeat();
+        this.getWord();
     }
+}
 
+class Game extends updatingReset{
+
+    // function for getting a random word from the database
+    
 
 }
 
-
-// console.log(newvariable.word)
-
-class keyboard extends SelectedWord{
-
-    updateKeyboard(s , node){
-        if(this.repeat[s.toLowerCase()] >= 0)
-        {
-            console.log(this.success);
-            if(this.repeat[s.toLowerCase()] > 0)
-            {
-                node.className += " rightLetter";
-                this.updateLetter(s);
-            }
-            else
-            {
-                node.className += " wrongLetter";
-                this.fail++;
-                this.updateFigure(this.fail);
-                this.updateWrongLetter(s);
-            }
-            this.repeat[s.toLocaleLowerCase()] = -1;
-            if(this.fail == 6){
-                alert('Sorry you are hanged !!!!');
-                this.reset();
-            }
-            else if(this.success == 0){
-                alert('yay you won !!!!');
-                this.reset();
-            }
-        }
-    }
-    digitalPress(){
-        const digitalKeyboard = document.querySelectorAll(".alphaKeys");
-        // console.log(digitalKeyboard);
-        digitalKeyboard.forEach((e)=>{
-            e.addEventListener('click',(x)=>{
-                this.updateKeyboard(e.textContent , e);
-                
-                
-            })
-        })
+class PlayerVerification extends Game {
+    initialisation(){
+        this.winCount = 0;
+        this.failedAttempt = 0;
+        this.userName;
+        this.password;
         return this;
     }
 
-    analogPress(){
-        document.addEventListener('keypress' , (e)=>{
-            if((e.charCode>64 && e.charCode <91) || (e.charCode >96 && e.charCode <123))
-            {
-                this.updateKeyboard(e.key , document.querySelector('.' + e.key.toLowerCase()));
-            }
-                // console.log(e.charCode);
-        })
+    login(userName, Password){
+
+    }
+    logout(){
+
     }
 }
 
 
-const newvariable = new keyboard;
-newvariable.initialisation().hideWord().noOfRepeat().digitalPress().analogPress();
+const newvariable = new PlayerVerification;
+newvariable.initialisation().getWord().digitalPress().analogPress();
