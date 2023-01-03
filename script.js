ff48c3ebfc159ed717774ca5022469966ff3fc56
Amal Shibu <87291689+AmalShibu888@users.noreply.github.com>
2023-01-03 16:10:11 +0000

    LATEST

--- script.js
+++ script.js
@@ -15,9 +15,21 @@ import { options } from "./database.js";
 const wordContainer = document.querySelector('.word');
 const digitalKeyboard = document.querySelectorAll(".alphaKeys");
  
+const signupBox = document.querySelector('.signuppop');
+const loginBox = document.querySelector('.loginpop');
+
+const LoginToSignupButton =  document.querySelector('.signupLink');
+const SignupToSignupButton =  document.querySelector('.loginLink');
+
+const BoxCloseButtons = document.querySelectorAll('.popup-close');
+
+const signupsubmitbutton = document.querySelector('.signupSubmitButton');
+const loginSubmitButton = document.querySelector('.loginSubmitButton');
+
+
+
 
 // class to select hide the word
-
 class SelectedWord{
 
     // get a random word from the database
@@ -70,8 +82,11 @@ class SelectedWord{
 
 
 
-
+// making digital and analog Keyboard responsive
 class keyboard extends SelectedWord{
+
+
+// making digital Keyboard responsive
     digitalKeyboard(){
         digitalKeyboard.forEach((e)=>{
             e.addEventListener('click',(x)=>{
@@ -82,11 +97,14 @@ class keyboard extends SelectedWord{
         })
         return this;
     }
+
+
+// making analog Keyboard responsive
     analogKeyboard(){
         document.addEventListener('keypress' , updateAnalogKeyboard);
         return this;
     }
-
+    // updating on the keyboard image
     updateKeyboard(s , node){
         if(newvariable.repeat[s.toLowerCase()] >= 0)
         {
@@ -115,6 +133,10 @@ class keyboard extends SelectedWord{
         }
     }
 }
+
+
+
+
 // updating the figure the analog keyboard and the wrong letters slots with each press
 class updatingReset extends keyboard{
 
@@ -128,22 +150,27 @@ class updatingReset extends keyboard{
             let x = document.querySelector('.try2');
             console.log(x);
             x.style.strokeWidth = "3";
+            x.style.stroke = "black";
         }
         else if(x == 3){
             let x = document.querySelector('.try3');
             x.style.strokeWidth = "3";
+            x.style.stroke = "black";
         }
         else if(x == 4){
             let x = document.querySelector('.try4');
             x.style.strokeWidth = "3";
+            x.style.stroke = "black";
         }
         else if(x == 5){
             let x = document.querySelector('.try5');
             x.style.strokeWidth = "3";
+            x.style.stroke = "black";
         }
         else{
             let x = document.querySelector('.try6');
             x.style.strokeWidth = "3";
+            x.style.stroke = "black";
         }
     }
 
@@ -204,16 +231,17 @@ class updatingReset extends keyboard{
 
 
 class PlayerVerification extends updatingReset {
+
+    // making all popup as well as buttons associated with it except submit button operational
     init(){
         this.logstat = 0;
         this.displaySignupLogin();
         this.closebutton();
+        this.switchbutton(LoginToSignupButton , signupBox);
+        this.switchbutton(SignupToSignupButton, loginBox);
     }
 
-  
-    logout(){
-
-    }
+//   validating the values written in submit form are allowable
     signupValidation(){
         const password = document.querySelector("#passworddec");
         const confpassword = document.querySelector("#confpassword");
@@ -225,6 +253,7 @@ class PlayerVerification extends updatingReset {
         return true;
     }
 
+    // making curnode capable of toggling the newnode popupbox
     switchbutton(curnode , newnode){
         curnode.addEventListener('click' , (e)=>{
             const par = curnode.parentNode;
@@ -233,9 +262,12 @@ class PlayerVerification extends updatingReset {
         })
         
     }
+
+
+    // making the close button on the top right corner of every popup box operational
     closebutton(){
-        const close_buttons = document.querySelectorAll('.popup-close');
-        close_buttons.forEach(button=> {
+        
+        BoxCloseButtons.forEach(button=> {
             // console.log("y");
                 button.addEventListener('click' , (e)=>{
                 document.addEventListener('keypress' , updateAnalogKeyboard);
@@ -245,54 +277,40 @@ class PlayerVerification extends updatingReset {
             })
         });
     }
+    
+    
+    // making the login/signup button(button) on top right corner of the screen operational ie toggle the respective form (box)
     buttonWork(button , box){
             button.addEventListener('click' ,(e) =>{
             document.removeEventListener('keypress' , updateAnalogKeyboard);
             const par =   box.parentNode;
             par.style.display = "flex";
             box.style.display = "block";
-    })
-}
-    signup(){
-        const signupButton = document.querySelector('.signup-button');
-        const signupBox = document.querySelector('.signuppop');
-        const curnode = document.querySelector('.loginLink');
-        const newnode = document.querySelector(".loginpop");
-        this.buttonWork(signupButton , signupBox);
-        this.switchbutton(curnode , newnode);
-        const signupsubmitbutton = document.querySelector('.signupSubmitButton');
+        })
+    }
 
+    // make signup possible
+    signup(){
         signupsubmitbutton.addEventListener('click' ,(e)=>{
             if(this.signupValidation())
             {
                 this.displayLogout();
                 signupBox.style.display = "none";
                 signupBox.parentElement.style.display = "none";
-                this.logout(); 
-                
+                this.logout();  
             }
             e.preventDefault();
         })
-        
-        
-        
         return this;
     }
+
+// make login possible
     login(){
-        const loginButton = document.querySelector('.login-button');
-        const loginBox = document.querySelector('.loginpop');
-        this.buttonWork(loginButton , loginBox);
-
-        const curnode = document.querySelector('.signupLink');
-        const newnode = document.querySelector(".signuppop");
-        this.switchbutton(curnode , newnode);
-
-        const loginForm = document.querySelector('.loginSubmitButton');
-        loginForm.addEventListener('click', (e)=>{
+        loginSubmitButton.addEventListener('click', (e)=>{
             e.preventDefault();
             this.displayLogout();
-            loginForm.parentElement.parentElement.style.display = "none";
-            loginForm.parentElement.style.display = "none";
+            loginBox.parentElement.style.display = "none";
+            loginBox.style.display = "none";
             this.logout();
         })
         
@@ -300,21 +318,16 @@ class PlayerVerification extends updatingReset {
         return this;
     }
 
+    // make logout possible
     logout(){
         const logoutTriggerButton = document.querySelector('.logout-button');
-        console.log(logoutTriggerButton)
         logoutTriggerButton.addEventListener('click', (e) =>{
             this.displaySignupLogin();
-            const loginButton = document.querySelector('.login-button');
-            const loginBox = document.querySelector('.loginpop');
-            this.buttonWork(loginButton , loginBox);
-            const signupButton = document.querySelector('.signup-button');
-            const signupBox = document.querySelector('.signuppop');
-            this.buttonWork(signupButton , signupBox);
         })
         return this;
     }
 
+    // display the signup/ligin button and make them responsive
     displaySignupLogin(){
         const par = document.querySelector('.loginSignup-button');
         par.innerHTML = ""
@@ -322,13 +335,17 @@ class PlayerVerification extends updatingReset {
         x.className = "login-button";
         x.textContent = "Login"
         par.appendChild(x);
+        this.buttonWork(x , loginBox);
         x = document.createTextNode('/');
         par.appendChild(x);
         x = document.createElement('span');
         x.className = "signup-button";
         x.textContent = "Signup"
         par.appendChild(x);
+        this.buttonWork(x , signupBox);
     }
+
+    // display logout button
     displayLogout(){
         const par = document.querySelector('.loginSignup-button');
         par.innerHTML = ""
@@ -357,7 +374,7 @@ newvariable.signup().login();
     
 
     
-
+// function to update when typing physically using keyboard
 
     function updateAnalogKeyboard(e){
         if((e.charCode>64 && e.charCode <91) || (e.charCode >96 && e.charCode <123))
