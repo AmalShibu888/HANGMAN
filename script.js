96e5e704d29afc720f865d04a125bbdcda3d37f5
Amal Shibu <87291689+AmalShibu888@users.noreply.github.com>
2023-01-06 18:23:42 +0000

    2

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 
 
 
-import { options } from "./database.js";
+// import { options } from "./database.js";
 
- 
+// const pool =  require("./module/database2.js");
+// console.log(pool);
+const { createPool } = require('mysql')
+
+const pool = createPool({
+    host: "localhost",
+    user: "root",
+    password: "leo10@messi",
+    database: "wordslist",
+    connectionLimit: 10
+})
+
+pool.query('select * from book1 ORDER BY RANd() LIMIT 1' ,(err ,result ,fields) =>{
+    if(err)
+        return console.log(err);
+    return console.log(result);
+})
 // readFile('test.txt' ,'utf8' ,(err ,data) =>{
 //     if(err){
 //         console.log(err);
